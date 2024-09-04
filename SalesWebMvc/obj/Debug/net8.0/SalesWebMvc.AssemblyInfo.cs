a91b78874c18629b85b800bfafd36eaa32ed2072
Leonardo Vieira <leonardovieira_94@hotmail.com>
2024-09-03 22:37:05 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3424cec376b57206beec736eae697ae9cb85d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3362fb820a3cda7ef4667f0dd70b3f680a7412b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
