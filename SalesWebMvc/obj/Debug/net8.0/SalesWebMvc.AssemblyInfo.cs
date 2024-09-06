66d2028e965bdf72d36a548a895f72c28ca60fa0
Leonardo Vieira <leonardovieira_94@hotmail.com>
2024-09-06 11:55:52 -0300

    Preparing sales search navigation views

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91b78874c18629b85b800bfafd36eaa32ed2072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a25b463930144f4e1c86f2cf08ec37592e9648")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
